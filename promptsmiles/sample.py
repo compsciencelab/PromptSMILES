9bd9a39bddfe68dbfcb43e2065e2a24442232559
MorganCThomas <morganthomas263@gmail.com>
2024-02-09 17:57:42 +0100

    detect existing fragments

--- promptsmiles/sample.py
+++ promptsmiles/sample.py
@@ -1,3 +1,7 @@
+# Copyright Universitat Pompeu Fabra 2020-2023  https://www.compscience.org
+# Distributed under the Apache 2.0 License.
+# (See accompanying file README.md file or copy at https://opensource.org/license/apache-2-0/)
+
 import random
 import warnings
 from copy import deepcopy
