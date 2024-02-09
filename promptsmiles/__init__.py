e148449e338e642f164962685094d4a797c6123a
MorganCThomas <morganthomas263@gmail.com>
2024-02-09 17:58:35 +0100

    license

--- promptsmiles/__init__.py
+++ promptsmiles/__init__.py
@@ -1,1 +1,5 @@
+# Copyright Universitat Pompeu Fabra 2020-2023  https://www.compscience.org
+# Distributed under the Apache 2.0 License.
+# (See accompanying file README.md file or copy at https://opensource.org/license/apache-2-0/)
+
 from promptsmiles.sample import DeNovo, ScaffoldDecorator, FragmentLinker
